e38caafa8daaed03d88ee242111a4b47b2745de9
Fabricio Santos <fabriciodacunhasantos@gmail.com>
2024-05-14 22:47:17 -0300

    changing tools

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -5,6 +5,7 @@ import { TranslateDirective } from 'app/shared/language';
   standalone: true,
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
   imports: [TranslateDirective],
 })
 export default class FooterComponent {}
